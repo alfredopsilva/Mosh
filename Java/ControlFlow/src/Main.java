3d1bc371b77dff0cfa7cf73ff9046a6ae83d599c
Alfredo <a.alfredops@gmail.com>
2022-12-12 19:37:29 -0500

    Finishing Mortgage Implementation | Creating a Montly Balance

---
+++ Java/ControlFlow/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 10;
+        while(i > 0)
+        {
+            System.out.println("Hello World");
+            i--;
+        }
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert a Number : ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
+        } else if (number % 5 == 0) {
+            System.out.println("Fizz");
+        } else
+        {
+            System.out.println(number);
+        }
+        */
+    }
+}
--- Java/MortgageCalculator/Mortgage/src/Main.java
+++ Java/MortgageCalculator/Mortgage/src/Main.java
@@ -2,34 +2,75 @@ import java.io.FileReader;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Quiz About Mortgage
-        System.out.print("Principal: ");
-        double Principal = scanner.nextDouble();
-        System.out.print("Annual Interest Rate: ");
-        double AnnualInterestRate = scanner.nextDouble();
-        System.out.print("Period in Months: ");
-        byte Period = scanner.nextByte();
 
-        // Calculating Mortgage
-        double Mortgage;
-        double calculatedInterest = (AnnualInterestRate / 12)/100;
-        double firstPart = calculatedInterest * Math.pow((1+calculatedInterest), Period);
-        double secondPart = Math.pow((1+calculatedInterest),Period) - 1;
-        Mortgage = Principal * (firstPart / secondPart);
+
+public class Main {
+
+    //Colors
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+        // Getting Mortgage information
+
+        double Principal  = gettingValues(1_000, 1_000_000, "Principal (1K - 1M) : " ,"Please , insert a value between $1,000 and $1,000,000" );
+        double AnnualInterestRate = gettingValues(0,30,"Annual Interest Rate: " , "Please insert a value between 0 and 30.");
+        short Period = (short)gettingValues(0,120,"Period in months : ", "Please, inter a value between 0 and 120 months.");
 
         // Displaying information
+        displayInfo(Principal, AnnualInterestRate, Period);
+
+    }
+
+    public static double calculateMortgage(double Principal, double annualInterestRate, short Period){
+        double Mortgage;
+        double calculatedInterest = (annualInterestRate / 12)/100;
+        double firstPart = calculatedInterest * Math.pow((1+calculatedInterest), Period);
+        double secondPart = Math.pow((1+calculatedInterest),Period) - 1;
+        return  Mortgage = Principal * (firstPart / secondPart);
+    }
+
+    public static double gettingValues(double min_limit, double max_limit, String prompt ,String message)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(prompt);
+        double value = scanner.nextDouble();
+
+        while(value < min_limit || value > max_limit )
+        {
+            System.out.println(message);
+            value = scanner.nextDouble();
+        }
+        return value;
+    }
+
+    public static void displayInfo(double Principal, double AnnualInterestRate, short Period)
+    {
         NumberFormat display = NumberFormat.getCurrencyInstance();
         NumberFormat displayPercent = NumberFormat.getPercentInstance();
-        System.out.println("-- Mortgage Information -- ");
+        System.out.println("-- MORTGAGE INFORMATION -- ");
         System.out.println("Your Principal value is " + display.format(Principal)
-                + ". Your annual interest rate selected was " + displayPercent.format(AnnualInterestRate / 100)
-                + ". You select a period of " + Period + " Years."
+                + ".\nYour annual interest rate selected was " + displayPercent.format(AnnualInterestRate / 100)
+                + ".\nYou select a period of " + Period + " Years."
         );
-        System.out.println("Considering this information , your Monthly Mortgage Payment is : " + display.format(Mortgage));
+        System.out.println("Considering this information , your monthly mortgage Payment is : " + ANSI_GREEN
+                + display.format(calculateMortgage(Principal,AnnualInterestRate,Period)) + ANSI_RESET);
+        System.out.println("-- Monthly Balance --");
+        System.out.println(montlyBalance(Principal,AnnualInterestRate,Period ));
+    }
 
+    public static double montlyBalance(double Principal, double annualInterestRate, short Period)
+    {
+        double balance=0;
+        double calculatedInterest = (annualInterestRate / 12)/100;
 
+        for(int i = 0; i < Period; i++){
+        balance = Principal*((Math.pow((1+calculatedInterest),Period) - Math.pow((1+calculatedInterest),i))
+                            /(Math.pow((1+calculatedInterest),Period) - 1));
+
+        }
+        return balance;
     }
 }
