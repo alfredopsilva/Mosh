c2c1afaf47e7b999fdfcb70c39bc75f1a8124bf8
Alfredo Silva <a.alfredops@gmail.com>
2023-08-02 13:58:25 -0400

    ReactJS => Building Components { Zod }

--- ReactJS/MoshDemo/src/components/Form.tsx
+++ ReactJS/MoshDemo/src/components/Form.tsx
@@ -1,34 +1,43 @@
-import React, { useRef } from 'react'
+import React, { FormEvent, useState } from 'react'
+import { z } from 'zod';
+import { FieldValues, useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"
+
+
+
+const schema = z.object({
+    name: z.string().min(3), 
+    age: z.number({invalid_type_error: 'Age Field is Required'}).min(1)
+})
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
 
-    const nameRef = useRef<HTMLInputElement>(null)
-    const ageRef = useRef<HTMLInputElement>(null)
-    const person = {name: '', age: 0}
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({resolver: zodResolver(schema)});
+    console.log(register("name"))
 
+    const onSubmit = (data: FieldValues) => {
+        console.log(data)
+    }
 
     return (
-        <form onSubmit={(event) => {
-            event.preventDefault(); 
-            console.log("Submited");
-            if(nameRef.current !== null && ageRef.current !== null)
-            {
-                person.name = nameRef.current.value;
-                person.age = parseInt(ageRef.current.value);
-                console.log(person)
-            }
-        }}>
-            <div className="mb-3">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3"></div>
                 <label htmlFor="name" className="form-label">Name</label>
-                <input ref={nameRef} id="name" type="text" className="form-control" />
+                <input {...register('name')} id="name" type="text" className="form-control" />
+                {errors.name && <p className='text-danger'>{errors.name.message}</p>}
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input ref={ageRef} id='age' type="number" className="form-control" />
+                <input {...register('age',{valueAsNumber: true})} id='age' type="number" className="form-control" />
+                {errors.age && <p className='text-danger'>{errors.age.message}</p>}
             </div>
             <button className="btn btn-primary" value="submit">Submit</button>
         </form>
     )
+
+
 }
 
 export default Form
